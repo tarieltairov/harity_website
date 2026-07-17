1bb1c0233bfa246d7e1b0e4f7ceab75eaab3f973
Jazgul <maratkyzyzazgul6@gmail.com>
2026-07-17 17:36:05 +0300

    fix: update FilterChip component

--- src/ui/FilterChip/FilterChip.tsx
+++ src/ui/FilterChip/FilterChip.tsx
@@ -2,16 +2,20 @@ import type { ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 import styles from "./FilterChip.module.scss";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+
+interface FilterChipProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isActive?: boolean
+}
 
 export function FilterChip({
   className,
   children,
+  isActive = false,
   ...props
-}: Props) {
+}: FilterChipProps) {
   return (
     <button
-      className={clsx(styles.filterChip, className)}
+      className={clsx(styles.filterChip, { [styles.active]: isActive }, className)}
       {...props}
     >
       {children}

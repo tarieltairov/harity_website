d606e435b3af74fa55e03d4d362035fffb70bc98
Nurlan1204 <skladnurlan@gmail.com>
2026-07-24 17:16:15 +0600

    feat: add StatBlock component

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Badge } from './ui/Badge';
 import { FilterChip } from './ui/FilterChip/FilterChip';
 import { Input, Textarea, SearchField } from './ui/form';
 import { Pagination } from './ui/Pagination';
+import { StatsBlock } from './components/StatBlock';
 
 function App() {
   return (
@@ -48,12 +49,14 @@ function App() {
           <Route path='/partners' element={<Partners />} />
           <Route path='/contacts' element={<Contacts />} />
         </Routes>
+        <StatsBlock />
  {/* 2. Временный тестовый контейнер для проверки инпутов из макета */}
       <div style={{ padding: '20px', maxWidth: '600px', display: 'flex', flexDirection: 'column', gap: '16px', margin: '20px auto', background: '#fff', borderRadius: '16px', boxShadow: '0 4px 12px rgba(0,0,0,0.05)' }}>
         <div style={{ display: 'flex', gap: '16px' }}>
           <Input placeholder="Ваше имя" />
           <Input type="email" placeholder="Email" />
         </div>
+        
         <SearchField placeholder="Поиск по новостям" />
         <Textarea placeholder="Сообщение" />
            </div>
